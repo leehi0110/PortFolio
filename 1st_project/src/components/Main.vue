67ef3cfdde7d4cb93bb64b10361792bea045d058
leehi0110 <maple0110@naver.com>
2020-05-19 17:32:49 +0900

    2020/05/19 1st_PortFolio

---
+++ 1st_project/src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mainContainer">
+    <div class="subContainer">
+      <Header class="headerComponent"></Header>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+  name: 'Main',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../scss/Main.scss';
+</style>
